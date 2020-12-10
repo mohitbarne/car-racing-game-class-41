0be14c828221c795728ec4d2f3d92d1d25bef5c2
mohitbarne <72339227+mohitbarne@users.noreply.github.com>
2020-12-10 16:48:07 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,50 @@
+var canvas, backgroundImage;
+
+var gameState = 0;
+var playerCount;
+var allPlayers;
+var distance = 0;
+var database;
+
+var form, player, game;
+
+var cars, car1, car2, car3, car4, carrank;
+
+var track, car1_img, car2_img, car3_img, car4_img;
+
+var rank1,rank2,rank3,carSound
+
+function preload(){
+  track = loadImage("images/track.jpg");
+  car1_img = loadImage("images/car1.png");
+  car2_img = loadImage("images/car2.png");
+  car3_img = loadImage("images/car3.png");
+  car4_img = loadImage("images/car4.png");
+  ground = loadImage("images/ground.png");
+  rank1 = loadImage("images/rank1.PNG");
+  rank2 = loadImage("images/rank2.PNG");
+  rank3 = loadImage("images/rank3.PNG");
+  carSound = loadSound("images/sound.wav");
+}
+
+function setup(){
+  canvas = createCanvas(displayWidth - 20, displayHeight-30);
+  database = firebase.database();
+  game = new Game();
+  game.getState();
+  game.start();
+}
+
+
+function draw(){
+  if(playerCount === 1){
+    game.update(1);
+  }
+  if(gameState === 1){
+    clear();
+    game.play();
+  }
+  if(gameState === 2){
+    game.end();
+  }
+}
